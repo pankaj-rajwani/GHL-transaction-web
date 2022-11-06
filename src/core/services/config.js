a8c93491ad2f4f53ad5092a35aa4b716f7063fb6
Pankaj Rajwani <rajwanipankaj23@gmail.com>
2022-11-06 17:02:57 +0530

    fix: fixes naming convention for env variable

--- src/core/services/config.js
+++ src/core/services/config.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 import { baseUrls } from './baseUrls';
 
 const instance = (serviceName) => {
-  console.log(process.env.REACT_APP_BASE_URL)
   return axios.create({
-    baseURL: `${process.env.REACT_APP_BASE_URL}${baseUrls[serviceName]}`,
+    baseURL: `${process.env.BACKEND_BASE_URL}${baseUrls[serviceName]}`,
   });
 };
 
