4b92763904337e53c78baf896bd4a1abefb7830f
Pankaj Rajwani <rajwanipankaj23@gmail.com>
2023-12-12 19:20:20 +0530

    Removes obsolete console statement

--- src/core/services/config.js
+++ src/core/services/config.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { baseUrls } from './baseUrls';
 
 const instance = (serviceName) => {
-  console.log(process.env.REACT_APP_BACKEND_BASE_URL)
   return axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND_BASE_URL}${baseUrls[serviceName]}`,
   });
